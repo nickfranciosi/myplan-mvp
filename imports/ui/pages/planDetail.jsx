8a942216f143a2d69cdf833d7d83d15862db23e7
Brad <bpotterbaum@gmail.com>
2017-11-15 21:50:10 -0600

    styled up the plan detail view

--- imports/ui/pages/planDetail.jsx
+++ imports/ui/pages/planDetail.jsx
@@ -8,18 +8,125 @@ import PlanCard from '../components/planCard';
 
 
 const PlanDetail = ({ plan = {} }) => (
-  <Section>
-    <PlanCard  {...plan}/>
-    <h3>Description: </h3>
-    <p>{plan.description}</p>
-    <a href={`/plan/${plan._id}/donate`}>Support this plan</a>
-  </Section>
+  <div>
+    <Section>
+      <H2>{plan.studentName} - {plan.school && plan.school.name}</H2>
+      <H1>{plan.planName}</H1>
+    </Section>
+    <Media src={plan.media} />
+    <Section>
+      <Stats>
+        <Stat color="#16C98D">
+          {plan.amountRaised}$320.00
+          <StatDescription>donations</StatDescription>
+        </Stat>
+        <Stat>
+          {plan.daysRemaining}4
+          <StatDescription>days to go</StatDescription>
+        </Stat>
+        <Stat>
+          {plan.supporterCount}24
+          <StatDescription>supporters</StatDescription>
+        </Stat>
+      </Stats>
+    </Section>
+    <Tabs>
+      <Tab border="2px solid #0080BB">Plan</Tab>
+      <Tab>Supporters</Tab>
+    </Tabs>
+    <Section>
+      <PlanDescription>{plan.description}</PlanDescription>
+    </Section>
+    <DonateBtn href={`/plan/${plan._id}/donate`} >Support this plan</DonateBtn>
+  </div>
 );
 
 const Section = styled.section`
   font-family: HelveticaNeue;
   padding: 0 20px;
-  margin-bottom: 26px;
+`;
+
+const H2 = styled.h2`
+  font-size: 16px;
+  color: #131313;
+  font-weight: normal;
+  padding: 26px 0 5px;
+`;
+
+const H1 = styled.h1`
+  font-family: HelveticaNeue-Bold;
+  font-weight: normal;
+  font-size: 28px;
+  color: #111111;
+  line-height: 32px;
+  padding-bottom: 46px;
+`;
+
+const Stats = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 46px 0;
+`;
+
+const Stat = styled.div`
+  font-family: HelveticaNeue-Medium;
+  font-size: 18px;
+  color: ${props => props.color || '#111111'};
+  flex: 0 1 auto;
+`;
+
+const Media = styled.div`
+  background-image:url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 190px;
+`;
+
+const StatDescription = styled.p`
+  font-family: HelveticaNeue;
+  font-size: 16px;
+  color: #626369;
+  padding-top: 6px;
+`;
+
+const Tabs = styled.div`
+  border-bottom: 1px solid #E6E7E8;
+  width: 100%;
+`;
+
+const Tab = styled.div`
+  display: inline-block;
+  margin-left: 20px;
+  font-family: HelveticaNeue-Bold;
+  font-size: 14px;
+  color: #626369;
+  padding: 0 0 14px;
+  border-bottom: ${props => props.border || 'none'};
+`;
+
+const PlanDescription = styled.div`
+  padding: 16px 0 90px;
+  font-family: HelveticaNeue;
+  font-size: 14px;
+  color: #626369;
+  line-height: 24px;
+`;
+
+const DonateBtn = styled.a `
+  display: block;
+  text-decoration: none;
+  background: #16C98D;
+  font-family: HelveticaNeue-Medium;
+  font-size: 16px;
+  color: #FFFFFF;
+  padding: 16px 0;
+  position: fixed;
+  bottom: 16px;
+  left: 20px;
+  right: 20px;
+  text-align: center;
 `;
 
 export default withTracker(props => {
